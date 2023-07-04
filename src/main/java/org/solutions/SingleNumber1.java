c79f1f1949f85e2079c3434bc39ca7636ded06dd
Vivek Singh <viveksinghinf@gmail.com>
2023-07-04 18:20:03 +0530

    leetcode solutions 04/07

---
+++ src/main/java/org/solutions/SingleNumber1.java
@@ -0,0 +1,11 @@
+package org.solutions;
+
+public class SingleNumber1 {
+    public int singleNumber(int[] nums) {
+        int answer = 0;
+        for(int num : nums) {
+            answer = answer^num;
+        }
+        return answer;
+    }
+}
