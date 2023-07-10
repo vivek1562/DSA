74d73a12ce4cdc90e769fc5de127ea03c54da10e
Vivek Singh <viveksinghinf@gmail.com>
2023-07-10 10:13:56 +0530

    LeetCode solutions 10/07

---
+++ src/main/java/org/solutions/InsertInterval.java
@@ -0,0 +1,30 @@
+package org.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertInterval {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> answer = new ArrayList<>();
+        int i=0;
+
+        while(i<intervals.length) {
+            if(newInterval[0] > intervals[i][1]) {
+                answer.add(intervals[i]);
+            } else if(newInterval[1] < intervals[i][0]) {
+                break;
+            } else {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+            i++;
+        }
+        answer.add(newInterval);
+        while(i<intervals.length) {
+            answer.add(intervals[i]);
+            i++;
+        }
+
+        return answer.toArray(new int[answer.size()][]);
+    }
+}
