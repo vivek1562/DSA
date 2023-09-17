5b198487f328661fe9923615086a6be2b07d9c89
Vivek Singh <40215271+vivek1562@users.noreply.github.com>
2023-09-17 23:20:52 +0530

    Time: 65 ms (17.41%), Space: 48.3 MB (24.07%) - LeetHub

---
+++ 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.java
@@ -0,0 +1,51 @@
+class Solution {
+    // class Node {
+    //     int value;
+    //     int mask;
+    //     int dist;
+
+    //     Node(int value, int mask, int dist) {
+    //         this.value = value;
+    //         this.mask = mask;
+    //         this.dist = dist;   
+    //     }
+    // }
+
+    public int shortestPathLength(int[][] graph) {
+        
+        int ans = 0, n = graph.length;
+        Set<Pair<Integer, Integer>> s = new HashSet<>();
+        Queue<int[]> q = new LinkedList<>();
+
+        if(n==0 || (n==1 && graph[0].length==0))
+            return 0;
+
+        for(int i=0; i<n; i++) {
+            q.offer(new int[] {i, 1<<i});
+            s.add(new Pair(i, 1<<i));
+        }
+        
+        while(!q.isEmpty()) {
+            int sz = q.size();
+            ans++;
+            while(sz-->0) {
+                int[] curr = q.poll();
+                int currNode = curr[0];
+                int mask = curr[1];
+
+                for(int node : graph[currNode]) {
+                    int newMask = mask | 1<<node;
+                    if(newMask == (1<<n)-1)
+                        return ans;
+
+                    if(!s.contains(new Pair(node, newMask))) {
+                        q.offer(new int[] {node, newMask});
+                        s.add(new Pair(node, newMask));
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
