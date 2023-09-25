46fe4b46fe96dde6b530209c0ca3cbba432de7f3
Vivek Singh <40215271+vivek1562@users.noreply.github.com>
2023-09-25 13:44:02 +0530

    Time: 78 ms (52.63%), Space: 60.6 MB (85.96%) - LeetHub

--- 1832-minimum-operations-to-make-a-subsequence/1832-minimum-operations-to-make-a-subsequence.java
+++ 1832-minimum-operations-to-make-a-subsequence/1832-minimum-operations-to-make-a-subsequence.java
@@ -3,26 +3,25 @@ class Solution {
         Map<Integer, Integer> targetMap = new HashMap<>();
         for(int i=0; i<target.length; i++)
             targetMap.put(target[i], i);
-        
+
         List<Integer> lis = new ArrayList<>();
         //loop over arr and do LIS in nlogn using binary search
-        for(int i=0; i<arr.length; i++) {
-            if(targetMap.containsKey(arr[i])) {
-                int lo = 0, hi = lis.size()-1, mid = 0, targetIdx = targetMap.get(arr[i]), lb = 0;
-                if(lis.isEmpty() || targetIdx>lis.get(hi)) {
+        for (int j : arr) {
+            if (targetMap.containsKey(j)) {
+                int lo = 0, hi = lis.size() - 1, mid = 0, targetIdx = targetMap.get(j);
+                if (lis.isEmpty() || targetIdx > lis.get(hi)) {
                     lis.add(targetIdx);
                     continue;
                 }
-                while(lo<=hi) {
-                    mid = lo+(hi-lo)/2;
-                    if(lis.get(mid)>=targetIdx) {
-                        lb = mid;
-                        hi = mid-1;
+                while (lo <= hi) {
+                    mid = lo + (hi - lo) / 2;
+                    if (lis.get(mid) < targetIdx) {
+                        lo = mid + 1;
                     } else {
-                        lo = mid+1;
+                        hi = mid - 1;
                     }
                 }
-                lis.set(lb, targetIdx);
+                lis.set(lo, targetIdx);
             }
         }
 
