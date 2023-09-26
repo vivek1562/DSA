3ff60a0b4f10c9e1ccc6377e0e8305c9a6f2bcd5
Vivek Singh <40215271+vivek1562@users.noreply.github.com>
2023-09-26 14:10:55 +0530

    Time: 2 ms (98.94%), Space: 40.3 MB (91.84%) - LeetHub

---
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        boolean[] vis = new boolean[26];
+        int[] lastIndexMap = new int[26];
+        char[] c = s.toCharArray();
+        for(int i=0; i<c.length; i++) {
+            lastIndexMap[c[i]-'a'] = i;
+        }
+        StringBuilder ans = new StringBuilder();
+        for(int i=0; i<c.length; i++) {
+            if(vis[c[i]-'a'] == false) {
+                while(!ans.isEmpty()) {
+                    char lastChar = ans.charAt(ans.length()-1);
+                    if(lastChar>c[i] && lastIndexMap[lastChar-'a']>i) {
+                        vis[lastChar-'a'] = false;
+                        ans.deleteCharAt(ans.length()-1);
+                    } else {
+                        break;
+                    }
+                }
+                ans.append(c[i]);
+                vis[c[i]-'a'] = true;
+            }
+        }
+        return ans.toString();
+    }
+}
