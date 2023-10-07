5fef90ecdf00ded779d68dacf390d3e55174a0f3
Vivek Singh <40215271+vivek1562@users.noreply.github.com>
2023-10-07 18:51:47 +0530

    Time: 73 ms (24.39%), Space: 48.8 MB (17.07%) - LeetHub

---
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -0,0 +1,37 @@
+class Solution {
+    Integer[][][] dp = new Integer[51][101][51];
+    int N, M, K;
+    int mod = (int)1e9+7;
+
+    public int numOfArrays(int n, int m, int k) {
+        N=n;
+        M=m;
+        K=k;
+        return recur(0, 0, 0);
+    }
+
+    public int recur(int i, int maxSoFar, int cost) {
+        if(i==N) {
+            if(cost==K)
+                return 1;
+            return 0;
+        }
+
+        if(dp[i][maxSoFar][cost]!=null)
+            return dp[i][maxSoFar][cost];
+        
+        if(cost>K)
+            return 0;
+
+        int ans = 0;
+        for(int j=1; j<=M; j++) {
+            if(j>maxSoFar) {
+                ans = (ans+recur(i+1, j, cost+1)%mod)%mod;
+            } else {
+                ans = (ans+recur(i+1, maxSoFar, cost)%mod)%mod;
+            }
+        }
+
+        return dp[i][maxSoFar][cost] = ans;
+    }
+}
