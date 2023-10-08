25e4e8f2a16485ed0b92182925cc1494044336ab
Vivek Singh <40215271+vivek1562@users.noreply.github.com>
2023-10-08 15:43:05 +0530

    Time: 108 ms (66.93%), Space: 44.4 MB (85.83%) - LeetHub

--- 2157-smallest-k-length-subsequence-with-occurrences-of-a-letter/2157-smallest-k-length-subsequence-with-occurrences-of-a-letter.java
+++ 2157-smallest-k-length-subsequence-with-occurrences-of-a-letter/2157-smallest-k-length-subsequence-with-occurrences-of-a-letter.java
@@ -1,35 +1,25 @@
 class Solution {
     public String smallestSubsequence(String s, int k, char letter, int repetition) {
-        int extraLetter = (int)s.chars().filter(ch -> ch==letter).count() - repetition, n = s.length(), remove = n-k;
+        int cnt = (int)s.chars().filter(ch -> ch==letter).count(), n = s.length(), remove = n-k, currCnt=0;
         StringBuilder st = new StringBuilder();
 
         for(int i=0; i<n; i++) {
-            while(st.length()>0 && st.charAt(st.length()-1)>s.charAt(i) && remove>0) {
-                if(st.charAt(st.length()-1)==letter) {
-                    if(extraLetter==0)
-                        break;
-                    else
-                        extraLetter--;
+            while(st.length()>0 && st.charAt(st.length()-1)>s.charAt(i) && (n-i+st.length())>k && (currCnt+cnt>repetition || st.charAt(st.length()-1)!=letter)) {
+                char ch = st.charAt(st.length()-1);
+                if(ch == letter) {
+                    currCnt--;
                 }
                 st.deleteCharAt(st.length()-1);
-                remove--;
             }
-            st.append(s.charAt(i));
+            if(st.length()<k && (s.charAt(i)==letter || k-st.length()>repetition-currCnt)) {
+                st.append(s.charAt(i));
+                if(s.charAt(i)==letter)
+                    currCnt++;
+            }
+            if(s.charAt(i)==letter)
+                cnt--;
         }
 
-        int i=0;
-        StringBuilder ans = new StringBuilder();
-        while(ans.length()<k) {
-            if(st.charAt(i) != letter && (ans.length()+Math.max(0, repetition) >= k)) {
-                i++;
-                continue;
-            }
-            if(st.charAt(i) == letter)
-                repetition--;
-            ans.append(st.charAt(i));
-            i++;     
-        }
-
-        return ans.toString();
+        return st.toString();
     }
 }
