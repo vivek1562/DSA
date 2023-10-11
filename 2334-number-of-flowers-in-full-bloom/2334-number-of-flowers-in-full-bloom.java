ee42361ebc7f852d103da1a1cce8b1583ed4ade4
Vivek Singh <40215271+vivek1562@users.noreply.github.com>
2023-10-11 15:51:07 +0530

    Time: 65 ms (61.54%), Space: 73.7 MB (24.23%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.java
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.java
@@ -4,18 +4,18 @@ class Solution {
 
         Arrays.sort(flowers, (a, b) -> (a[0]-b[0]));
         for(int i=0; i<ans.length; i++) {
-            ans[i] = Math.max(upperBound(flowers, people[i]), 0);
+            ans[i] = Math.max(lowerBound(flowers, people[i]), 0);
         }
-        System.out.println("");
+
         Arrays.sort(flowers, (a, b) -> (a[1]-b[1]));
         for(int i=0; i<ans.length; i++) {
-            ans[i] -= Math.max(lowerBound(flowers, people[i])+1, 0);
+            ans[i] -= Math.max(upperBound(flowers, people[i])+1, 0);
         }
 
         return ans;
     }
 
-    public int upperBound(int[][] flowers, int target) {        //gives us the index of largest element(flowers[0]) smaller than or equal to the target
+    public int lowerBound(int[][] flowers, int target) {        //gives us the index of smallest element(flowers[0]) greater than the target. So index-0 == flowers bloomed
         int lo = 0, hi = flowers.length-1, mid = 0;
 
         while(lo<=hi) {                 
@@ -26,11 +26,11 @@ class Solution {
                 hi = mid-1;
             }
         }
-        System.out.println(lo);
+        
         return lo;
     }
 
-    public int lowerBound(int[][] flowers, int target) {        //gives us the index of largest element(flowers[1]) smaller than the target
+    public int upperBound(int[][] flowers, int target) {        //gives us the index of largest element(flowers[1]) smaller than the target
         int lo = 0, hi = flowers.length-1, mid = 0;
 
         while(lo<=hi) {
@@ -41,7 +41,7 @@ class Solution {
                 lo = mid+1;
             }
         }
-        System.out.println(hi);
+        
         return hi;
     }
 }
