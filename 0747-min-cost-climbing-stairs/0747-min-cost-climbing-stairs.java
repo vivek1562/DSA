42d74bcc8477d6ef9e1738b4450a9e1323f255ba
Vivek Singh <40215271+vivek1562@users.noreply.github.com>
2023-10-14 13:19:38 +0530

    Time: 1 ms (46.18%), Space: 42.7 MB (85.86%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    int[] dp;
+    
+    public int minCostClimbingStairs(int[] cost) {
+        dp = new int[cost.length];
+        Arrays.fill(dp, -1);
+        int cost0 = recur(0, cost);
+        Arrays.fill(dp, -1);
+        int cost1 = recur(1, cost);
+        return Math.min(cost0, cost1);
+    }
+
+    public int recur(int i, int[] cost) {
+        if(i>=cost.length)
+            return 0;
+        if(dp[i]!=-1)
+            return dp[i];
+        return dp[i] = cost[i]+Math.min(recur(i+1, cost), recur(i+2, cost));
+    }
+}
